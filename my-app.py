312d7054197c7a12e354d5f50f79589c20e4f66c
junaidmalik1997 <108491775+junaidmalik1997@users.noreply.github.com>
2022-08-04 06:01:49 +0500

    Update my-app.py

--- my-app.py
+++ my-app.py
@@ -1,17 +1,9 @@
-# from multiprocessing import Process
-
-# server = Process(target=app.run)
-# server.start()
-# # ...
-# server.terminate()
-# server.join()
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello world333333'
+    return 'hello world3333334444444444444'
 
 app.run(host='0.0.0.0', port=82)
