e0f765a3f3ff6ebbaef14e23224a4ebaec76009d
junaidmalik1997 <108491775+junaidmalik1997@users.noreply.github.com>
2022-08-11 01:53:08 +0500

    Update my-app.py

--- my-app.py
+++ my-app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello slaves'
+    return 'hello00000000000000000000000'
 
-app.run(host='0.0.0.0', port=5004)
+app.run(host='0.0.0.0', port=5001)
